1c6ae27f99bbc8c37b6d63a185d7847f112cd0f1
Samir_Tak <samir.tak@brainvire.com>
2023-09-12 17:41:31 +0530

    Fixed Bugs

--- pos_training/models/models.py
+++ pos_training/models/models.py
@@ -7,23 +7,12 @@ from odoo.exceptions import ValidationError
 class pos_training(models.Model):
     _inherit=['pos.order']
 
-    phoneNumber = fields.Char(string="Phone Number")
+    phoneNumber = fields.Char(string="Order Note")
     
-    # @api.constrains('phoneNumber')
-    def search_phone(number):
-        # for rec in self:
-        #     record = self.search_count(
-        #         [('phoneNumber', '=', rec.phoneNumber), ('id', '!=', rec.id)])
-        #     if record > 0:
-        #         raise ValidationError(
-        #             "Number Already Exists!")
-        return number    
     @api.model
     def _order_fields(self, ui_order):
         res = super(pos_training, self)._order_fields(ui_order)
-        # print("raw res" ,res)
         res['phoneNumber'] = ui_order.get('phoneNumber') if ui_order.get('phoneNumber') else False
-        # print("updated res" ,res)
         return res
     
     
